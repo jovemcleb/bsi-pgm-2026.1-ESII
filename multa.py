2600f9ff952e8792a9ed0e0b7136cbca0887845f
Caleb Lima <lima.cardoso.caleb@gmail.com>
2026-06-09 21:08:15 -0300

    feat: multa zero quando sem atraso (ciclo 1)

---
+++ multa.py
@@ -0,0 +1,5 @@
+def calcular_multa_com_carencia(dias_atraso, valor_dia, carencia=2):
+    _ = dias_atraso
+    _ = valor_dia
+    _ = carencia
+    return 0.0
---
+++ tests/test_multa.py
@@ -0,0 +1,16 @@
+"""TODO list do kata de multa com carencia.
+
+- atraso 0 dias -> multa 0,00
+- atraso alem da carencia -> cobra os dias excedentes
+- atraso dentro da carencia -> multa 0,00
+- valor por dia configuravel
+- arredondar para 2 casas
+"""
+
+from multa import calcular_multa_com_carencia
+
+
+def test_sem_atraso_nao_gera_multa():
+    multa = calcular_multa_com_carencia(dias_atraso=0, valor_dia=10.0)
+
+    assert multa == 0.0
